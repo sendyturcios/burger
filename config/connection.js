070cdce33376cee3b81813c84e1d31b6af56e0ea
sturcios <sen.turcios@gmail.com>
2018-08-11 14:10:57 -0500

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ let connection;
 
 if(process.env.JAWSDB_URL) {
     console.log('heroku')
-    connection = mysql.createConnection(process.env.JAMSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 } else {
     console.log('local')
